9ce8ade6f6d487c3855c6360901bb728460385c8
Noah Altunian <nbaltunian@gmail.com>
2022-03-19 14:49:56 -0400

    feat: Create landing page with wallet connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // Use the connectWallet hook to connect our wallet.
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log(`👋 Address: ${address}`);
+
+  // Prompt the user to connect their wallet if it has not been done.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to The District DAO</h1>
+        <h2>Home of the native Washington, DC cryptocurrency and Decentralized Autonomous
+          Organization <a href="https://ethereum.org/en/dao/" target="_blank" rel="noreferrer">(DAO)</a>.</h2>
+
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // Return if the user's wallet is connected.
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected</h1>
     </div>
   );
 };
